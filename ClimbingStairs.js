e3b39694ae2691b03b3fb5b6eb2208cc17e8ca0c
tarabaramaksym <tarabara.maksym@protonmail.com>
2022-08-08 20:12:58 +0300

    Day five

---
+++ ClimbingStairs.js
@@ -0,0 +1,17 @@
+
+var climbStairs = function (n) {
+
+  // 1 1 1
+  // 1 2
+  // 2 1
+
+  let arr = [];
+  arr[1] = 1;
+  arr[2] = 2;
+  for (let i = 3; i <= n; i++) {
+    arr[i] = arr[i - 1] + arr[i - 2];
+  }
+  return arr[n];
+};
+
+console.log(climbStairs(5));
